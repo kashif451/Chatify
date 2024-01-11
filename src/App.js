efe41d5a828d95c7098facd76e2d43347f270f2e
Jamshaid Ahmed Khan <official.jimmi666@gmail.com>
2024-01-12 03:13:28 +0500

    React Router Setup Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
 import Home from './pages/home';
 import Login from './pages/login';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Register from './pages/register';
+
+const router=createBrowserRouter([
+  {
+    path:'/register',
+    element: <Register/>
+  },
+  {
+    path:'/login',
+    element: <Login/>
+  },
+  {
+    path:'/chatify',
+    element: <Home/>
+  },
+  {
+    path:'/',
+    element: <h1>HELLO</h1>
+  },
+])
 
 function App() {
   return (
    <>
-   <div className='register'>
-   {/* <Register/> */}
-   {/* <Login/> */}
-   <Home/>
+   <div >
+   <RouterProvider router={router}/>
    </div>
    </>
   );
